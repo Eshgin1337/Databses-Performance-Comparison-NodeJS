51c9c618d4b319e0992ca5d34fae21876eb89c6e
Eshgin <71690707+Eshgin1337@users.noreply.github.com>
2023-10-10 11:34:16 +0400

    MySQL Script for inserting created

---
+++ mysql-API.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+
+const app = express();
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12345",
+    database: "users"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE if not exists users (email VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+    });
+});
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+
+
+app.post("/signup", function (req, res) {
+    email = req.body.email;
+    password = req.body.password;
+
+    var insertScript = `insert into users (email, password) values (${email}, ${password})`;
+    con.query(insertScript, function (err, result) {
+        if (err) throw err;
+        console.log("Row inserted");
+    });
+});
+
+
+app.listen(3000, function (req, res) {
+    console.log("Server started on port 3000");
+});
